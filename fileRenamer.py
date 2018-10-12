f8453d35f96ff895db19ef18aacf9773ae2261a5
osman23 <shaheer.osman@uoit.net>
2018-10-12 09:31:21 -0400

    Update fileRenamer.py

--- fileRenamer.py
+++ fileRenamer.py
@@ -4,7 +4,7 @@ import os
 fileLocation='the location of the file you want to rename'
 os.chdir(fileLocation)
 
-#this increments the filenames by 5 ex: newFileName005,newFileName010
+#this increments the filenames by 5 e.g: newFileName005,newFileName010
 x=5
 x=str (x)
 
